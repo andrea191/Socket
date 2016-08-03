7f56864bd7245c60e5afbc80f026791eb74f63bf
andrea191 <andrea.m.91@gmail.com>
2016-08-03 11:12:40 +0300

    second

--- Client/broadcastClient.py
+++ Client/broadcastClient.py
@@ -1,13 +1,13 @@
 # Send UDP broadcast packets
 
-MYPORT = 50000
+MYPORT = 10000
 
-import sys, time
-from socket import *
+import sys, time, socket
 
-s = socket(AF_INET, SOCK_DGRAM)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(('', 0))
-s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
 while 1:
     data = repr(time.time()) + '\n'
